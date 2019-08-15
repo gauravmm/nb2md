b91140679f4db76190fc01dca857adc3d52a94c3
Gaurav Manek <gaurav@gauravmanek.com>
2019-08-14 21:48:49 -0400

    Partial

---
+++ formatting.py
@@ -0,0 +1,85 @@
+# Formatting Code for nb2md
+
+class GFMFormatter(object):
+    def __init__(self, external_image):
+        self.external_image = external_image
+
+    def markdown(cell):
+        cell_text = "".join(cell["source"])
+        cell_text = re.sub(r'!\[(.*)\]\((.*)\)', 
+                            r'{% include image.html img="\2" caption="\1"%}', 
+                            cell_text)
+
+        cell_text = re.sub(r'<center>(.*?)</center>',
+                            r'<div class="center-text" markdown="1">\n\1\n</div>',
+                            cell_text)
+
+        cell_text = escape_symbols(cell_text)
+
+        cell_text = re.sub(r'\\begin{equation}(.*?)\\end{equation}',
+                            r'\n$$\1$$\n',
+                            cell_text, flags=re.DOTALL)
+
+        return cell_text
+
+    def preamble(cell, download_link):
+        return f"""---
+layout: post
+title: cell["source"][1][2:]
+subtitle: cell["source"][2][2:]
+author: cell["source"][3][2:]
+img: cell["source"][4][2:]
+---
+
+[Download post as jupyter notebook]({download_link})
+
+"""
+
+    def source(cell):
+    return f"""```python
+{"".join(cell["source"])}
+```"""
+
+    def output(cell):
+        for mime, handler in cell_output_handlers:
+            if mime in data:
+                return handler(data, self.external_image)
+
+
+# Similar to GFM, but without a preamble and image substitution:
+class DiderotFormatter(GFMFormatter):
+    def markdown(cell):
+        cell_text = "".join(cell["source"])
+        cell_text = escape_symbols(cell_text)
+        cell_text = re.sub(r'\\begin{equation}(.*?)\\end{equation}',
+                            r'\n$$\1$$\n',
+                            cell_text, flags=re.DOTALL)
+        return cell_text
+
+    def preamble(cell, download_link):
+        return None
+
+
+# Utility functions:
+def escape_symbols(text):
+    """ Replace \{  with \\{ within $'s.
+        Replace * with \* within $'s """
+    new_text = ""
+    regex = r"[^\\]\$(?:[^\$\\]|\\.)*?\$"
+    pos = 0
+    m = re.search(regex, text[pos:])
+    while m:
+        new_text += text[pos:pos+m.start()]
+
+        new_text += re.sub(r"(\\[\{\}\|]|[\*\_\|])", r"\\\1",m.group(0))
+        pos += m.end()
+        m = re.search(regex, text[pos:])
+    new_text += text[pos:]
+    return new_text
+
+cell_output_handlers = OrderedDict([
+    ("text/html",     lambda d, img: f"<div><small>{''.join(d)}</small></div>"),
+    ("image/svg+xml", lambda d, img: img.process("image/svg+xml", d)),
+    ("image/png",     lambda d, img: img.process("image/png", d)),
+    ("text/plain",    lambda d, img: f"<pre>{''.join(d)}</pre>"),
+    ("text",          lambda d, img: f"<pre>{''.join(d)}</pre>")])
--- nb2md.py
+++ nb2md.py
@@ -1,130 +1,75 @@
 #!/usr/bin/env python
 
-import os
-import re
 import argparse
 import json
-import glob
-import shutil
+from collections import OrderedDict
+from pathlib import Path
 
-# Eric: PIL images for PNG cell outputs
-from PIL import Image
-from io import BytesIO
-import base64
 
-def escape_symbols(text):
-    """ Replace \{  with \\{ within $'s.
-        Replace * with \* within $'s """
-    new_text = ""
-    regex = r"[^\\]\$(?:[^\$\\]|\\.)*?\$"
-    pos = 0
-    m = re.search(regex, text[pos:])
-    while m:    
-        new_text += text[pos:pos+m.start()]
+class ExternalImage(object):
+    def __init__(self, flavour="gfm"):
+        self.images = []
 
-        new_text += re.sub(r"(\\[\{\}\|]|[\*\_\|])", r"\\\1",m.group(0))
-        pos += m.end()
-        m = re.search(regex, text[pos:])
-    new_text += text[pos:]
-    return new_text
+    def.process(self, mimetype, imagedata):
+        if mimetype == "image/svg+xml":
+            file = f"output_{len(self.images)}.svg"
+            # Prepare the svg file for writing:
+            self.images.append((file, imagedata.encode()))
+            return f'{{% include image.html img="{file}" %}}'
+
+        elif mimetype == "image/png":
+            # Inline
+            return f'<div><img src="data:image/png;base64, {}"></div>\n'.format(imagedata))
+
+
+def main(args):
+    data = json.loads(args.nb_file.read_text())
+    external_image = ExternalImage(flavour=args.flavour)
+
+    output_file = parser.output if parser.output else args.nb_file.with_suffix(".md")
+    if output_file.suffix != ".md":
+        output_file = output_file / args.nb_file.with_suffix(".md")
+    output_file.mkdir(parents=True, exist_ok=True)
+
+    output = []
+
+    for cell in data["cells"]:
+        if cell["cell_type"] == "markdown":
+            output.append(cell_markdown(cell))
+
+        elif cell["cell_type"] == "code":
+            if len(cell["source"]) == 0:
+                continue
+            elif cell["source"][0].startswith("### PREAMBLE"):
+                cell_preamble(cell, output_file.with_suffix(".tar.gz"))
+            else:
+                output.append(cell_source(cell))
+
+                if "outputs" in cell:
+                    # Find the output types we want and select the first matching type.
+                    output_types_index = [t, output_types_index[t]
+                        for t in ["display_data", "execute_result", "stream"]
+                        if t in { out["output_type"]:i for i, out in enumerate(cell["outputs"]) }]
+
+                    try:
+                        output_cell_type, output_cell_index = output_types_index
+                    except IndexError:
+                        continue
+
+                    output.append(cell_output_markdown(cell["outputs"][output_cell_index]["data"], external_image))
+
+    output_file.write_text("\n".join((o for o in output if o is not None)))
+    external_image.write()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--svg_scale", type=float, default=0.75)
-    parser.add_argument("nb_file", type=str, help="jupyter notebook file")
-    args = parser.parse_args()
+    parser.add_argument("nb_file", type=Path, help="jupyter notebook file")
+    parser.add_argument(["--output", "-o"], default=Path("output/"), type=Path, help="output path, will be clobbered")
 
-    base, ext = os.path.splitext(args.nb_file)
+    flavor = parser.add_mutually_exclusive_group(required=False)
+    flavor.add_argument("--gfm", metavar="flavor", type="store_const", const="gfm", help="GitHub flavored markdown")
+    flavor.add_argument("--diderot", metavar="flavor", type="store_const", const="diderot", help="Diderot-compatible markdown")
 
-    with open(args.nb_file) as f:
-        raw_data = f.read()
-        f.seek(0)
-        data = json.load(f)
-
-    output = open(base + ".md", "w")
-
-    output_idx = 0
-
-    for cell in data["cells"]:
-        if cell["cell_type"] == "markdown":
-            cell_text = "".join(cell["source"]) + "\n"
-            cell_text = re.sub(r'!\[(.*)\]\((.*)\)', 
-                               r'{% include image.html img="\2" caption="\1"%}', 
-                               cell_text)
-
-            cell_text = re.sub(r'<center>(.*?)</center>',
-                               r'<div class="center-text" markdown="1">\n\1\n</div>',
-                               cell_text)
-
-            cell_text = escape_symbols(cell_text)
-
-            cell_text = re.sub(r'\\begin{equation}(.*?)\\end{equation}',
-                               r'\n$$\1$$\n',
-                               cell_text, flags=re.DOTALL)
-
-
-            output.write(cell_text + "\n")
-
-        elif cell["cell_type"] == "code":
-            print(len(cell["source"]))
-            if len(cell["source"]) > 0 and cell["source"][0].startswith("### PREAMBLE"):
-                output.write("---\nlayout: post\ntitle: {}subtitle: {}author: {}img: {}---\n\n[Download post as jupyter notebook]({})\n\n"\
-                    .format(cell["source"][1][2:], cell["source"][2][2:], 
-                            cell["source"][3][2:], cell["source"][4][2:], base + ".tar.gz"))
-            else:
-                cell_text = "".join(cell["source"]) + "\n"
-                output.write("\n```python\n" + cell_text + "```\n\n")
-
-                if "outputs" in cell:
-                    output_types = {out["output_type"]:i 
-                                    for i,out in enumerate(cell["outputs"])}
-                    if "display_data" in output_types:
-                        # write svg to file and link to the svg
-                        print("here")
-                        cell_output = cell["outputs"][output_types["display_data"]]
-                        if "image/svg+xml" in cell_output["data"]:
-                            with open("output_{}.svg".format(output_idx), "w") as f_svg:
-                                f_svg.writelines(cell_output["data"]["image/svg+xml"])
-                        output.write('\n{{% include image.html img="output_{}.svg" %}}\n\n'.format(output_idx))
-                        output_idx += 1
-
-                    elif "execute_result" in output_types:
-                        # print execution results (mainly tables)
-                       
-                        cell_output = cell["outputs"][output_types["execute_result"]]
-                        if "text/html" in cell_output["data"]:
-                            cell_output_text = "".join(cell_output["data"]["text/html"])
-                            output.write("\n<div><small>" + cell_output_text + "</small></div>\n")
-                        elif "image/svg+xml" in cell_output["data"]:
-                            with open("output_{}.svg".format(output_idx), "w") as f_svg:
-                                f_svg.writelines(cell_output["data"]["image/svg+xml"])
-                            output.write('\n{{% include image.html img="output_{}.svg" %}}\n\n'.format(output_idx))
-                            output_idx += 1
-                        elif "image/png" in cell_output["data"]: 
-                            cell_data = cell_output["data"]["image/png"]
-                            output.write('\n<div><img src="data:image/png;base64, {}"></div>\n'.format(cell_data))
-                        elif "text/plain" in cell_output["data"]:
-                            cell_output_text = "".join(cell_output["data"]["text/plain"])
-                            output.write("<pre>\n" + cell_output_text + "\n</pre>\n\n")
-
-                    elif "stream" in output_types:
-                        cell_output = cell["outputs"][output_types["stream"]]
-                        cell_output_text = "".join(cell_output["text"])
-                        output.write("<pre>\n" + cell_output_text + "</pre>\n\n")
-
-    output.close()
-
-    ## create the notebook directory and 
-    all_files = glob.glob("**/*", recursive=True)
-    occuring_files = [f for f in all_files if raw_data.find(f) >= 0 and os.path.isfile(f) and os.stat(f).st_size < 20*1024*1024]
-    if os.path.exists(base):
-        shutil.rmtree(base)
-    os.makedirs(base)
-    for f in occuring_files + [base + ".ipynb"]:
-        shutil.copy(f, base)
-    os.system("tar -czf {0}.tar.gz {0}".format(base))
-
-    base_post = os.path.split(os.getcwd())[1]
-    shutil.move(base + ".md", "../_posts/" + base_post + ".md")
-
-
+    main(parser.parse_args())
